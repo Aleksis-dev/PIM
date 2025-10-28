838884e2d6c3e5a36b712058a610379fae3cf9f7
Aleksis-dev <lastovskis08@gmail.com>
2025-10-28 14:49:28 +0200

    Initialized new laravel api project and created AuthController

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request) {
+        try {
+            $validated = $request->validate([
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'errors' => $e->errors()
+            ], 422);
+        }
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if ($user && $user->password === $validated['password']) {
+            $userTokens = $user->tokens();
+            if ($userTokens) {
+                $userTokens->delete();
+            }
+            $token = $user->createToken("ADMIN_TOKEN")->plainTextToken;
+            return response()->json([
+                'token' => $token
+            ], 200);
+        } else {
+            return response()->json([
+                'errors' => [
+                    'credentials' => 'Invalid Credentials!'
+                ]
+            ], 422);
+        }
+    }
+}
